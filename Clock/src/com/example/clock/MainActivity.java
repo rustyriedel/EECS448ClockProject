91ad99caaabbc121eeabc9f5428c41af255a3a8a
Alec Knutsen <aqknutsen@gmail.com>
2016-02-12 15:27:38 -0600

    Add comments and javadoc comments

--- Clock/src/com/example/clock/MainActivity.java
+++ Clock/src/com/example/clock/MainActivity.java
@@ -1,129 +1,161 @@
+
 package com.example.clock;
 
-import android.support.v7.app.AppCompatActivity;
-import android.app.Activity;
-import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
-import android.widget.NumberPicker;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.RadioButton;
+import android.support.v7.app.AppCompatActivity; // Base class for android activities
 
-import java.lang.reflect.Array;
+import android.os.Bundle; // Class that maps string values
+
+import android.os.Handler; // Handler class is used to run the timer in a seperate thread from the main thread
+
+import android.util.Log; // Class imported for debugging messages
+
+import android.view.Menu; // Default import for android menus
+import android.view.MenuItem; // Default import for android menus
+
+import android.view.View; // Class that is the base block for the user interface
+
+import android.widget.NumberPicker; // Class used for swipable number pickers
+import android.widget.RadioGroup;
+import android.widget.TextView; // Class used for text view of timer
+import android.widget.RadioButton; // Class used for radio buttons
 
 
+/**
+ * Main Activity  is the activity in which a timer is displayed, swipers allow the user to set the time, and radio buttons allow the user to swtich between 12/24 hour mdoe and AM/PM.
+ * @author     Cesar Avalos
+ * @author     John Russel
+ * @author     Yarden Tamir
+ * @author     Alec Knutsen
+ * @version    First Deployment
+ * @since       1.0
+ */
 public class MainActivity extends AppCompatActivity {
 
-	private String class_name;
+	private String class_name; // String for class name for debugging purposes
 
-	public static boolean my_bool;
+	private TextView showtime = null; // Textview used to actually display the time
 
+	private NumberPicker hourPicker = null; // Swipable interface for setting the hours
+	private NumberPicker minutePicker = null; // Swipable interface for setting the minutes
+	private NumberPicker secondPicker = null; // Swipable interface for setting the seconds
 
-	public TextView showtime;
+	private RadioGroup am_pm = null; // Radio group to select am or pm
+	private RadioGroup  twelve_twentyfour = null; // Radio group to select time mode
 
-	public String hours;
-	public String minutes;
-	public String seconds;
-	public String mode;
+	private Timer my_time = null; // Decleration of a variable with type Timer (see Timer class)
 
-	public Spinner changehour;
-	public Spinner changeminutes;
-	public Spinner changeseconds;
-	public Spinner changemode;
+	/*
+	 * Handler is used to update the timer display in real time in a seperate thread then the main thread.
+	 * Handler is used so that the main thread does not become blocked and unresponsive.
+	 */
+	private Handler handler =null;
 
-	public NumberPicker hourPicker;
-	public NumberPicker minutePicker;
-	public NumberPicker secondPicker;
-
-
-	protected Timer my_time;
-
-
-
-	protected Handler handler =null;
-
-	protected updateTimer my_update;
-
+	/**
+	 * This class implements the run method from the Runnable interface. This class allows the Handler class to queue calls to the run method, so that the timer display can be updated in a seperate thread.
+	 * @version    First Deployment
+	 * @since       1.0
+	 */
 	class updateTimer implements Runnable {
 
+
+		/**
+		 * Implemnts the run method of the runnable interface
+		 * Sets the time in a textbox and on teh swipable hours, minutes, and seconds UI features. Then, delays one second.
+		 * Uses methods from Timer.java to display time and update time
+		 * @see             Timer.java
+		 * @since           1.0
+		 */
 		public void run() {
 
-			showtime.setText(my_time.display());
+			showtime.setText(my_time.display()); // Set the text display by using the display method of the timer class
+			hourPicker.setValue(my_time.getHour()); // Set the swipable hourPicker to the current hour
+			minutePicker.setValue(my_time.getMinute()); // Set the swipable hourPicker to the current minute
+			secondPicker.setValue(my_time.getSecond()); // Set the swipable hourPicker to the current second
 
-			if(handler != null) {
+			if(handler != null) { // If the handler is not null we delay one full second and then update the time again
+
 				handler.postDelayed(this, 1000);
-				hourPicker.setValue(my_time.getHour());
-				minutePicker.setValue(my_time.getMinute());
-				secondPicker.setValue(my_time.getSecond());
+
 			}
 
 		}
 	}
 
+	private updateTimer my_update = null; // Instace of the updateTimer class
 
+
+
+	/**
+	 * Creates an instance of the Hander Class and updateTimer class.
+	 * Queues calls to the run method from the updateTimer clsas every one second.
+	 * @see             updateTimer
+	 * @since           1.0
+	 */
 	public void callHandler() {
 
-		handler = new Handler();
+		handler = new Handler(); // Create a new instance of the handler
 
-		my_update = new updateTimer();
+		my_update = new updateTimer(); // Create a new instance of the update_timer
 
-		handler.postDelayed(my_update,1000);
+		handler.postDelayed(my_update,1000); // Queue calls to the run method every 1 second
 	}
 
+	/**
+	 * Destroys all calls to the run method in the Handler Queue. Resets the Handler class to point at null.
+	 * @see             updateTimer
+	 * @since           1.0
+	 */
 	public void removeHandler() {
 
-		if(handler == null) {
+		if(handler != null) { // If the handler is not null
 
-		}
-
-		else {
-			handler.removeCallbacks(my_update);
-			handler =null;
+			handler.removeCallbacks(my_update); // Remove any queued calls to run
+			handler =null; // Set the handler object to null
 		}
 	}
 
 
-	/*
-	 * Constructor the stores the Class_Name for debugging purposes 
+	/**
+	 * Constructor used to store the class name
+	 * @since           1.0
 	 */
 	public MainActivity() {
 
-		class_name = getClass().getName();
+		class_name = getClass().getName(); // Store teh class name
 
 	}
 
+	/**
+	 * This method checks if one of the AM/PM radio buttons is clicked. It uses the setAMPM method of the timer class to update the timer accordingly
+	 *
+	 * @param view      Represents the view of the radio button
+	 * @see Timer
+	 * @since           1.0
+	 */
 	public void onRadioButtonClicked2(View view) {
 		// Is the button now checked?
-		boolean checked = ((RadioButton) view).isChecked();
+		boolean checked = ((RadioButton) view).isChecked(); // Boolean that stores if one of the radio button's is checked
 
 		// Check which radio button was clicked
 		switch (view.getId()) {
 
 			case R.id.radio_PM:
-				if (checked) {
+				if (checked) { // If the pm button is checked
 
-					if (my_time.getAMPM().equals("AM")) {
+					if (my_time.getAMPM().equals("AM")) { // Check if the previous mode is AM
 
-
-						my_time.setAMPM("PM");
-
+						my_time.setAMPM("PM"); // Switch the mode
 
 					}
 				}
 				break;
+
 			case R.id.radio_AM:
-				if (checked) {
+				if (checked) { // If the am button is checked
 
-					if (my_time.getAMPM().equals("PM")) {
+					if (my_time.getAMPM().equals("PM")) { // Check if the previous mode is PM
 
-						my_time.setAMPM("AM");
+						my_time.setAMPM("AM"); // Switch to Am
 
 					}
 
@@ -132,65 +164,106 @@ public class MainActivity extends AppCompatActivity {
 		}
 	}
 
+	/**
+	 * This method checks if one of the AM/PM radio buttons is clicked. It uses getters, setters from the Timer class to appropriately to convert the time between the two modes
+	 *
+	 * @param view      Represents the view of the radio button
+	 * @see Timer
+	 * @since           1.0
+	 */
 	public void onRadioButtonClicked(View view) {
-		// Is the button now checked?
-		boolean checked = ((RadioButton) view).isChecked();
+
+		boolean checked = ((RadioButton) view).isChecked(); // Boolean that stores if one of the buttons is checked
 
 		// Check which radio button was clicked
 		switch(view.getId()) {
 
 			case R.id.radio_12:
 
-				hourPicker.setMaxValue(12);
-				hourPicker.setMinValue(1);
-				if (checked)
-					
+				if (checked) // If 12 hour mode is checked
+
+					//Set the max min value of the hour selector to be between 1-12
+					hourPicker.setMaxValue(12);
+					hourPicker.setMinValue(1);
+
+					//12 PM corresponds to 12 military time
 					if(my_time.getHour() == 12)
 					{
 						my_time.setAMPM("PM");
 					}
-				
+
+					//Anytime greater than 13 in military time, is 13-12 PM in regular time
 					else if(my_time.getHour() >= 13) {
-						my_time.setHour(my_time.getHour()-12);
-						my_time.setAMPM("PM");
+
+						my_time.setHour(my_time.getHour()-12); // Set hour
+						my_time.setAMPM("PM"); // Set to PM
+
 					}
 
+					//Anything below 13 corresponds to the same hour in regular time except when hours is 0
 					else {
+
+						// If the hours is 0, set the time to 12 AM
 						if(my_time.getHour()==0) {
-							my_time.setHour(12);
+
+							my_time.setHour(12); // Set hour to 12
+
 						}
 
-						my_time.setAMPM("AM");
+						my_time.setAMPM("AM"); // Set to AM
 						
 						
 					}
-				my_time.setMode(true);
+
+				my_time.setMode(true); // Set mode to 12 hour, so that the timer display can be updated properly
 
 				break;
+
 			case R.id.radio_24:
-				hourPicker.setMaxValue(23);
-				hourPicker.setMinValue(0);
-				if (checked)
+
+				if (checked) // If the 24 hour button is checked
+
+					//Change the hour selector, so that the user can select between 0 and 23 hours
+					hourPicker.setMaxValue(23);
+					hourPicker.setMinValue(0);
+
+					// If we are in PM mode in 24 hour we convert the time appropriately
 					if(my_time.getAMPM()=="PM") {
+
+						//12 PM in regular time converts to 12 in military time
 						if(my_time.getHour() ==12) {
-							my_time.setHour(12);
+
+							my_time.setHour(12); // Set hours to 12
+
 						}
+
+						//Any other PM time in regular time besides 12 converts to the number of hours +12 in military time
 						else {
-							my_time.setHour(my_time.getHour() + 12);
+
+							my_time.setHour(my_time.getHour() + 12); // Set hours to current hours + 12
 
 						}
 
-						my_time.setAMPM("PM");
+						my_time.setAMPM("PM"); // Set the current mode to PM so we know what mode we are in
 
 					}
+
+				//If the current 12hour mode is AM
 				if(my_time.getAMPM()=="AM") {
+
+					// 12 AM corrspoends to 0 military time
+
 					if(my_time.getHour() == 12) {
-						my_time.setHour(0);
+
+						my_time.setHour(0); // Set hours to 0
+
 					}
 
-					my_time.setAMPM("AM");
+					my_time.setAMPM("AM"); // Set mode to AM
 				}
-				my_time.setMode(false);
+
+				my_time.setMode(false); // Set mode so that we are in 24 hour mode
+
 				break;
 
 
@@ -199,56 +272,63 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 
+
+	/**
+	 * onCreate is called when the app is first loaded. We initialize all variables and start the Handler.
+	 *
+	 * @param savedInstanceState     Default parameter set by android
+	 * @see
+	 * @since           1.0
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
+		//Default android calls to parent class
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		Log.d(class_name, "Creating Main Activity");
+		//Initialize radio groups
+		am_pm = (RadioGroup) findViewById(R.id.radioGroup2);
+		twelve_twentyfour = (RadioGroup) findViewById(R.id.radioGroup);
+
+		//Set radio groups so that we start in AM and 12 hour mode
+		am_pm.check(R.id.radio_AM);
+		twelve_twentyfour.check(R.id.radio_12);
 
 
 
-
-		showtime = (TextView) findViewById(R.id.timer);
-
-		my_time = new Timer(0,0,12);
-
-		callHandler();
-
+		// Initialize swipable hour picker
 		hourPicker = (NumberPicker) findViewById(R.id.numberPickerHour);
 		hourPicker.setMaxValue(12);
 		hourPicker.setMinValue(1);
 
+		// Initialize swipable minute picker
 		minutePicker = (NumberPicker) findViewById(R.id.numberPickerMin);
 		minutePicker.setMaxValue(59);
 
+		// Initialize swipable second picker
 		secondPicker = (NumberPicker) findViewById(R.id.numberPickerSec);
 		secondPicker.setMaxValue(59);
 
+		// This sets the hours using the setHour method of the Timer class when the user swipes to a new hour
 		hourPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
 			@Override
 			public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-
-
 				my_time.setHour(newVal);
-
-
-
 			}
 		});
 
+		// This sets the minutes using the setMinutes method of the Timer class when the user swipes to a new minute
 		minutePicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
 			@Override
 			public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
 
-
 				my_time.setMinute(newVal);
-
 
 			}
 		});
 
+		// This sets the seconds using the setSeconds method of the Timer class when the user swipes to a new second
 		secondPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
 			@Override
 			public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
@@ -258,12 +338,28 @@ public class MainActivity extends AppCompatActivity {
 			}
 		});
 
+		// Initalize timer clock
+		showtime = (TextView) findViewById(R.id.timer);
 
+		//Set initial time to be 12 AM
+		my_time = new Timer(0,0,12);
+
+		// This starts queing call to the run method which runs until removeHandler is called in onDestroy
+		callHandler();
+
+
+		//Debug message
+		Log.d(class_name, "Creating Main Activity");
 
 	}
 
 
-
+	/**
+	 * Method created automatically by android
+	 *
+	 * @param menu    Default parameter set by android
+	 * @since           1.0
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -271,6 +367,12 @@ public class MainActivity extends AppCompatActivity {
 		return true;
 	}
 
+	/**
+	 * Method created automatically by android
+	 *
+	 * @param item    Default parameter set by android
+	 * @since           1.0
+	 */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
@@ -283,66 +385,78 @@ public class MainActivity extends AppCompatActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	/**
+	 * Method created automatically by android
+	 *
+	 * @since           1.0
+	 */
 	@Override
 	protected void onStart() {
 
-
 		super.onStart(); // Make sure the code in the parent class is used
 
-
-
-		Log.d(class_name, "Starting Main Activity");
-
-
+		Log.d(class_name, "Starting Main Activity"); // Log debug message
 	}
 
 
+	/**
+	 * Method created automatically by android
+	 *
+	 * @since           1.0
+	 */
 	@Override
 	protected void onResume() {
 
-
 		super.onResume(); // Call code of parent class
 
-		Log.d(class_name, "Resuming Main Activity");
+		Log.d(class_name, "Resuming Main Activity"); // Log debug message
 
 	}
 
-
+	/**
+	 * Method created automatically by android
+	 *
+	 * @since           1.0
+	 */
 	@Override
 	protected void onPause() {
 
-
 		super.onPause(); // Call parent code 
 
-		Log.d(class_name, "Pausing Main Activity");
-
-
+		Log.d(class_name, "Pausing Main Activity"); // Log debug message
 
 	}
 
 
+	/**
+	 * Method created automatically by android
+	 *
+	 * @since           1.0
+	 */
 	protected void onStop() {
-
 
 		super.onStop(); // Call parent code 
 
-
-		Log.d(class_name, "Stopping Main Activity");
-
-
+		Log.d(class_name, "Stopping Main Activity"); // Log debug message
 
 	}
 
 
+	/**
+	 * Method created automatically by android. Call removeHandler() to destroy the Handler
+	 *
+	 * @see removeHandler()
+	 * @since           1.0
+	 */
 	@Override
 	protected void onDestroy() {
 
 
 		super.onDestroy();
 
-		removeHandler();
+		removeHandler(); // Destroy handler and any remaining calls to run when the activity is destroyed
 
-		Log.d(class_name, "Destroying Main Activity ");
+		Log.d(class_name, "Destroying Main Activity "); // Log debug message
 
 	}
 
--- Clock/src/com/example/clock/Timer.java
+++ Clock/src/com/example/clock/Timer.java
@@ -1,238 +1,285 @@
 package com.example.clock;
 
 
-import android.util.Log;
-
+/**
+ * Timer is a helper class for main activity. All class variables have getter, setter methods to update timing variables.
+ * Furthermore, this class containts two methods which update the time and return a string which represents the curren time.
+ * @author     Cesar Avalos
+ * @author     John Russel
+ * @author     Yarden Tamir
+ * @author     Alec Knutsen
+ * @version    First Deployment
+ * @since       1.0
+ */
 public class Timer {
 
-	private String class_name;
+	//Stores time values for hours, seconds, and minutes
+	private int current_hour;
+	private int current_minute;
+	private int current_second;
 
-	protected int starting_hour;
-	protected int starting_minute;
-	protected int starting_second;
+	//Boolean that stores value for 12/24 mode. True = 12 hour mode, False = 24 hour mode
+	private boolean mode12hour =true;
 
-	protected int current_hour;
-	protected int current_minute;
-	protected int current_second;
-
-	protected int num_times_display =0;
-
-	protected boolean mode12hour =true;
-
-	protected String am_pm = "AM";
+	//String that stores either AM/PM
+	private String am_pm = "AM";
 
 
 
-
-
-
+	/**
+	 * This constructor initalizes the class variables <code>current_second, current hour, current minute</code>.
+	 * This constructor will be called in onCreate() of the MainActivity
+	 * @param second - Represents initial seconds when the program is first called
+	 * @param minute - Represents initial minutes when the program is first called
+	 * @param hour - Represents initial hours when the pogram is first called
+	 * @since       1.0
+	 */
 	public Timer(int second, int minute, int hour) {
 
-		class_name = getClass().getName();
-
-		starting_second = second;
-		starting_minute = minute;
-		starting_hour = hour;
-
+		// Initalize starting time values to those past in to constructor
 		current_second = second;
 		current_minute = minute;
 		current_hour = hour;
 
 	}
 
+	//Getters
 
+
+	/**
+	 * Get method for class variable am_pm
+	 * @return am_pm - Returns class variable am_pm
+	 * @since       1.0
+	 */
+	public String getAMPM() {return(am_pm);}
+
+	/**
+	 * Get method for class variable current_hour
+	 * @return am_pm - Returns class variable current_hour
+	 * @since       1.0
+	 */
+	public int getHour()
+	{
+		return current_hour;
+	}
+
+	/**
+	 * Get method for class variable current_minute
+	 * @return am_pm - Returns class variable current_minute
+	 * @since       1.0
+	 */
+	public int getMinute()
+	{
+		return current_minute;
+	}
+
+	/**
+	 * Get method for class variable current_second
+	 * @return am_pm - Returns class variable current_second
+	 * @since       1.0
+	 */
+	public int getSecond()
+	{
+		return current_second;
+	}
+
+
+	/**
+	 * Get method for class variable mode12hour
+	 * @return am_pm - Returns class variable mode12hour
+	 * @since       1.0
+	 */
+	public boolean getMode() {return(mode12hour);}
+
+
+	//Setters
+
+
+	/**
+	 * Set method for class variable mode12hour
+	 * @param  mode - Class variable mode12hour is set to mode
+	 * @since       1.0
+	 */
+	public void setMode(boolean mode) {mode12hour = mode;}
+
+	/**
+	 * Set method for class variable current_hour
+	 * @param hour - Class variable current_hour is set to hour
+	 * @since       1.0
+	 */
+	public void setHour(int hour)
+	{
+		current_hour = hour;
+	}
+
+	/**
+	 * Set method for class variable current_minute
+	 * @param min - Class variable current_minute is set to min
+	 * @since       1.0
+	 */
+	public void setMinute(int min)
+	{
+		current_minute = min;
+	}
+
+	/**
+	 * Set method for class variable current_second
+	 * @param sec - Class variable current_second is set to sec
+	 * @since       1.0
+	 */
+	public void setSecond(int sec)
+	{
+		current_second = sec;
+	}
+
+	/**
+	 * Set method for class variable am_pm
+	 * @param x - Class variable am_pm is set to x
+	 * @since       1.0
+	 */
+	public void setAMPM(String x) {am_pm = x; }
+
+
+	/**
+	 * This method is a manual program of a clock. It takes in the previous hours, minutes, seconds and updates them accordingly
+	 * @param previous_seconds - Represents the clocks current seconds
+	 * @param previous_minutes - Represents the clocks current minutes
+	 * @param previous_hour - Represents the clocks current hour
+	 * @return int[] - This method returns the updated hours,minutes, seconds and stores them in an int arrays
+	 * @since       1.0
+	 */
 	public int[] updateTime(int previous_seconds, int previous_minutes, int previous_hour) {
 
-		int[] my_time_array = new int[3];
+		int[] my_time_array = new int[3]; // Time array to store updated time values
 
+		//Variables that will temporarily store updated time values
 		int next_seconds = previous_seconds;
 
 		int next_minutes = previous_minutes;
 
 		int next_hour = previous_hour;
 
+		//If we are not at 59 seconds, just update the seconds +1
 		if(previous_seconds!= 59) {
 
 			next_seconds = previous_seconds +1;
+
 		}
 
+		//If the seconds is 59, go into this else if statement
 		else if (previous_seconds ==59) {
 
-			next_seconds = 0;
+			next_seconds = 0; // Reset the seconds to 0
 
+			//If we are not at 59 minutes,update the minutes +1
 			if(previous_minutes!= 59) {
+
 				next_minutes = previous_minutes +1;
+
 			}
 
+			//If we are at 59 minutes, go into this elseif statement
 			else if(previous_minutes == 59) {
-				next_minutes =0;
 
-				if(mode12hour == true) {
+				next_minutes =0; // Reset the minutes to 0
 
-					if(previous_hour!=12) {
+				//If we are in 12 hour mode, go into this if statement
+				if(getMode() == true) {
 
-						if (previous_hour ==11) {
-							if(am_pm =="AM") {
+					if(previous_hour!=12) { // If the previous hour is not 12, go into this if statement
+
+						if (previous_hour ==11) { // If the previous hour is 11, swtich from either AM to PM or PM to AM
+
+							if(am_pm =="AM") { // If we are at AM, reset to PM
 
 								am_pm = "PM";
+
 							}
 
-							else if (am_pm == "PM") {
+							else if (am_pm == "PM") { // If we are at PM, reset to AM
 								am_pm = "AM";
+
 							}
-							next_hour = previous_hour +1;
+
+							next_hour = previous_hour +1; // Update next hour +1
+
 						}
 
-						else if (previous_hour ==12) {
-
-
+						else if (previous_hour ==12) { // If the previous hour is at 12, reset hour to 1
 
 
 							next_hour = 1;
+
 						}
-
-
 
 					}
 
 				}
 
 
-				else if (mode12hour ==false) {
+				else if (mode12hour ==false) { // If we are in 24 hour mode, go into this else if block
 
-					if(previous_hour!=23) {
+					if(previous_hour!=23) { // If we are not at hour 23, update hour +1
 
 						next_hour = previous_hour +1;
+
 					}
 
-					else if (previous_hour ==23) {
+
+					else if (previous_hour ==23) { // If we are at hour 23, reset hour to 0
 
 						next_hour = 0;
 					}
-				}
 
+				}
 
 			}
 
 		}
 
+		//Store updated time values in array
 		my_time_array[0] = next_seconds;
 		my_time_array[1] = next_minutes;
 		my_time_array[2] = next_hour;
 
+		//Return array
 		return(my_time_array);
 
-
-	}
-
-	//Getters
-
-	public String getAMPM() {
-		return(am_pm);
-	}
-
-	public int getHour()
-	{
-		return current_hour;
-	}
-
-	public int getMinute()
-	{
-		return current_minute;
-	}
-
-	public int getSecond()
-	{
-		return current_second;
-	}
-	public boolean getMode() {return(mode12hour);}
-
-
-
-	public void setMode(boolean mode)
-	{
-		mode12hour = mode;
-	}
-
-	public void setHour(int hour)
-	{
-		current_hour = hour;
-	}
-
-	public void setMinute(int min)
-	{
-		current_minute = min;
-	}
-
-	public void setSecond(int sec)
-	{
-		current_second = sec;
-	}
-
-	public void setAMPM(String x) {
-		Log.d(class_name, "Set AM PM");
-		am_pm = x;
 	}
 
 
-
+	/**
+	 * This method calls the updateTime() method to get the current time, then returns a String with the displayed time.
+	 * @return String - Returns the time display represented as a string
+	 * @see updateTime()
+	 * @since       1.0
+	 */
 	public String display()  {
 
-		String display;
+			String display; // String that will be time display
 
+			int[] my_time_array = updateTime(current_second,current_minute,current_hour); // Call update time method to update time
 
-
-		if(num_times_display == 0) {
-
-			if(mode12hour == true) {
-				display = String.format("%d", current_hour) + ":" + String.format("%02d", current_minute) + ":" + String.format("%02d", current_second) +" " + getAMPM();
-				num_times_display =1;
-			}
-
-			else {
-
-				display = String.format("%d", current_hour) + ":" + String.format("%02d", current_minute) + ":" + String.format("%02d", current_second);
-				num_times_display =1;
-
-			}
-
-
-
-		}
-
-		else {
-
-			int[] my_time_array = new int[3];
-
-			my_time_array = updateTime(current_second,current_minute,current_hour);
-
+			//Store newly updated time
 			current_second = my_time_array[0];
 			current_minute = my_time_array[1];
 			current_hour = my_time_array[2];
 
+			//Display time with AM/PM since mode is 12 hour
 			if(mode12hour==true) {
 				display = String.format("%d", current_hour) + ":" + String.format("%02d", current_minute) + ":" + String.format("%02d", current_second) +" " + getAMPM();
 
 			}
 
+			//Display time without AM/PM since mode is 24 hour
 			else {
 				display = String.format("%d", current_hour) + ":" + String.format("%02d", current_minute) + ":" + String.format("%02d", current_second);
 
 			}
 
 
-		}
-
-
-
-
-
-
-
-		return(display);
-
+		return(display); // Return the string to display
 
 	}
-
 
 }
