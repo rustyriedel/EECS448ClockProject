ff9c244b463712a51bfbc47af5f342c72607e39a
Dillon Fruhwirth <d487f080@ku.edu>
2016-03-02 21:44:09 -0600

    Initial commit

---
+++ Clock/src/com/example/clock/Stopwatch.java
@@ -0,0 +1,15 @@
+package com.example.clock;
+
+
+/**
+ * Stopwatch is a helper class for main activity. All class variables have getter, setter methods to update timing variables.
+ * Furthermore, this class containts two methods which update the time and return a string which represents the time.
+ * @author     Dillon Fruhwirth
+ * @version    First Deployment
+ * @since       1.0
+ */
+public class Stopwatch {
+
+    
+
+}
